91eabc3e43c054aa7bde9b9d7f6f1d9c13c1117b
Tama-B <tama.britsch@gmail.com>
2022-04-12 10:18:59 +0200

    game start begonnen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,21 @@
 import { fly } from "./abilities.js";
 import { handleObstacles } from "./obstacles.js";
 
-
 const gameloop = () => {
     fly()
     handleObstacles();
 }
 
-let intervalId = setInterval(() => {
-    gameloop()
-}, 20)
+const startGameLoop = () => {
+    let intervalId = setInterval(() => {
+        gameloop()
+    }, 20)
+}
+
+const startGame = () => {
+    startGameLoop();
+}
+
 
 
 
--- assets/js/obstacles.js
+++ assets/js/obstacles.js
@@ -1,59 +1,88 @@
 import { currentHeight } from "./abilities.js";
 
-
 const gameContainer = document.querySelector('.game_container');
 
 const obstacleStartPosition = 1040;
-let currentObstaclePosition = obstacleStartPosition;
-const obstacleDespawnPosition = -40;
-const obstacleSpeed = 5;
-let isSpawned = false;
 
-const randomizeObstacleHeight = (min, max) => {
+const obstacleDespawnPosition = -100;
+const obstacleSpeed = 5;
+
+
+const randomNumberBetween = (min, max) => {
     return Math.random() * (max - min) + min;
 }
 
+const randomColor = () => {
+    let hexCode = Math.floor(Math.random() * 16777215).toString(16);
+    return `#${hexCode}`
 
-
-const spawnObstacle = () => {
-    let obstacle = document.createElement('div');
-    obstacle.classList.add('obstacle');
-    let obstacleHeight = randomizeObstacleHeight(100, 300);
-    obstacle.style.height = `${obstacleHeight}px`;
-    gameContainer.appendChild(obstacle);
-    obstacle.style.left = `${obstacleStartPosition}px`;
-    isSpawned = true;
 }
 
-const despawnObstacle = (obstacle) => {
-    if (currentObstaclePosition <= obstacleDespawnPosition) {
-        obstacle.remove();
-        currentObstaclePosition = obstacleStartPosition;
-        isSpawned = false;
+
+class Obstacle {
+    constructor(height, width, color) {
+        this.height = height;
+        this.width = width;
+        this.color = color;
+    };
+    currentPosition = obstacleStartPosition;
+    spawn() {
+        let element = document.createElement('div');
+        element.classList.add('obstacle');
+        element.style.height = `${this.height}px`;
+        element.style.width = `${this.width}px`;
+        element.style.backgroundColor = `${this.color}`;
+        gameContainer.appendChild(element);
+        element.style.left = `${obstacleStartPosition}px`;
+        return element;
     }
+
 }
 
-const moveObstacles = (obstacle) => {
-    currentObstaclePosition -= obstacleSpeed;
-    obstacle.style.left = `${currentObstaclePosition}px`
+let spawnedObstacles = [];
+let randomObstacleNumber = 0;
+
+setInterval(() => {
+    randomObstacleNumber = randomNumberBetween(2000, 4000);
+    const obs = new Obstacle(randomNumberBetween(100, 300), randomNumberBetween(40, 100), randomColor());
+    const htmlElement = obs.spawn();
+    let obstacleElementAndObject = [htmlElement, obs];
+    spawnedObstacles.push(obstacleElementAndObject);
+}, 4000);
+
+const despawnObstacles = () => {
+    spawnedObstacles.forEach(element => {
+        if (element[1].currentPosition <= obstacleDespawnPosition) {
+            element[0].remove();
+            spawnedObstacles.shift();
+        }
+
+    });
+}
+
+const moveObstacles = () => {
+    spawnedObstacles.forEach(element => {
+        element[1].currentPosition -= obstacleSpeed;
+        element[0].style.left = `${element[1].currentPosition}px`;
+    });
+
 }
 
 export const handleObstacles = () => {
-    if (isSpawned == false) { spawnObstacle(); };
-    if (isSpawned == true) {
-        const obstacle = document.querySelector('.obstacle');
-        moveObstacles(obstacle);
-        despawnObstacle(obstacle);
-        detectCollision();
-    }
+    moveObstacles();
+    detectCollision();
+    despawnObstacles();
 }
+
 
 /////////////////////////Collision
 
 const detectCollision = () => {
-    if (currentObstaclePosition <= 260 &&
-        currentHeight <= 160 &&
-        currentObstaclePosition >= 120) {
-        console.log('Collision!');
-    }
+    spawnedObstacles.forEach(element => {
+        if (element[1].currentPosition <= 240 &&
+            currentHeight - 50 <= element[1].height &&
+            element[1].currentPosition + element[1].width >= 120) {
+        }
+    });
+
 }
