31cd3bf0bc06c81e9f2431e3db76133f4daea75c
Tama-B <tama.britsch@gmail.com>
2022-04-12 14:28:23 +0200

    gameloop is working game playable!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 import { fly } from "./abilities.js";
-import { handleObstacles } from "./obstacles.js";
+import { handleObstacles, startObstacleLoop, stopObstacleSpawning, clearObstacles } from "./obstacles.js";
+
+const gameContainer = document.querySelector('.game_container');
+const body = document.querySelector('body');
+const startBtn = document.querySelector('.start_btn');
+const overlay = document.querySelector('.overlay');
+let intervalId
 
 const gameloop = () => {
     fly()
@@ -7,14 +13,38 @@ const gameloop = () => {
 }
 
 const startGameLoop = () => {
-    let intervalId = setInterval(() => {
+    intervalId = setInterval(() => {
         gameloop()
     }, 20)
 }
 
 const startGame = () => {
+    clearObstacles();
     startGameLoop();
+    startObstacleLoop();
+    console.log('Game started');
+    overlay.classList.add('hidden');
+    startBtn.disabled = true;
+
 }
+
+startBtn.addEventListener('click', startGame);
+
+const stopGameLoop = () => {
+    clearInterval(intervalId);
+    console.log('You failed!');
+}
+
+export const endGame = () => {
+    stopGameLoop();
+    stopObstacleSpawning();
+    overlay.classList.remove('hidden');
+    startBtn.disabled = false;
+    let text = startBtn.textContent;
+    startBtn.textContent = 'Play Again';
+}
+
+
 
 
 
--- assets/js/obstacles.js
+++ assets/js/obstacles.js
@@ -1,24 +1,26 @@
 import { currentHeight } from "./abilities.js";
+import { endGame } from "./main.js";
 
 const gameContainer = document.querySelector('.game_container');
 
 const obstacleStartPosition = 1040;
-
 const obstacleDespawnPosition = -100;
 const obstacleSpeed = 5;
+let intervalId
 
-
+//Create random number for obstacle height, width etc.
 const randomNumberBetween = (min, max) => {
     return Math.random() * (max - min) + min;
 }
 
+//Create random color for obstacles
 const randomColor = () => {
     let hexCode = Math.floor(Math.random() * 16777215).toString(16);
     return `#${hexCode}`
 
 }
 
-
+//Class for Obstacles
 class Obstacle {
     constructor(height, width, color) {
         this.height = height;
@@ -40,26 +42,46 @@ class Obstacle {
 }
 
 let spawnedObstacles = [];
+
 let randomObstacleNumber = 0;
 
-setInterval(() => {
-    randomObstacleNumber = randomNumberBetween(2000, 4000);
-    const obs = new Obstacle(randomNumberBetween(100, 300), randomNumberBetween(40, 100), randomColor());
-    const htmlElement = obs.spawn();
-    let obstacleElementAndObject = [htmlElement, obs];
-    spawnedObstacles.push(obstacleElementAndObject);
-}, 4000);
 
+//Create Obstacles and add them to DOM
+export const startObstacleLoop = () => {
+    intervalId = setInterval(() => {
+        randomObstacleNumber = randomNumberBetween(2000, 4000);
+        const obs = new Obstacle(randomNumberBetween(100, 300), randomNumberBetween(40, 100), randomColor());
+        const htmlElement = obs.spawn();
+        let obstacleElementAndObject = [htmlElement, obs];
+        spawnedObstacles.push(obstacleElementAndObject);
+    }, 4000);
+
+}
+
+
+//Stop spawning obstacles when game is over
+export const stopObstacleSpawning = () => {
+    clearInterval(intervalId);
+}
+
+//Despawn obstacle when it leaves game container
 const despawnObstacles = () => {
     spawnedObstacles.forEach(element => {
         if (element[1].currentPosition <= obstacleDespawnPosition) {
             element[0].remove();
             spawnedObstacles.shift();
         }
-
     });
 }
 
+export const clearObstacles = () => {
+    spawnedObstacles.forEach(element => {
+        element[0].remove();
+    });
+    spawnedObstacles.length = 0;
+}
+
+//Move obstacles from right to left
 const moveObstacles = () => {
     spawnedObstacles.forEach(element => {
         element[1].currentPosition -= obstacleSpeed;
@@ -67,6 +89,7 @@ const moveObstacles = () => {
     });
 
 }
+
 
 export const handleObstacles = () => {
     moveObstacles();
@@ -82,6 +105,7 @@ const detectCollision = () => {
         if (element[1].currentPosition <= 240 &&
             currentHeight - 50 <= element[1].height &&
             element[1].currentPosition + element[1].width >= 120) {
+            endGame();
         }
     });
 
